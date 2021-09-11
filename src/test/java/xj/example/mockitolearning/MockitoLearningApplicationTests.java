66fbcc40568a88ff970d439626d89013da7e23b9
xujie <jie.xu@thoughtworks.com>
2021-09-11 20:45:21 +0800

    init

---
+++ src/test/java/xj/example/mockitolearning/MockitoLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package xj.example.mockitolearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
